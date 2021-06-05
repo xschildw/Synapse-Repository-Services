ba660b4cadfc82bc0ded3b55d26217bd0d924bf8
Xavier Schildwachter <x.schildwachter@sagebase.org>
2021-06-04 18:30:55 -0700

    Use annotation?

--- lib/lib-audit/src/main/java/org/sagebionetworks/audit/kinesis/ObjectRecordLoggerImpl.java
+++ lib/lib-audit/src/main/java/org/sagebionetworks/audit/kinesis/ObjectRecordLoggerImpl.java
@@ -3,9 +3,11 @@ package org.sagebionetworks.audit.kinesis;
 import org.sagebionetworks.kinesis.AwsKinesisFirehoseLogger;
 import org.sagebionetworks.kinesis.AwsKinesisLogRecord;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class ObjectRecordLoggerImpl implements ObjectRecordLogger {
 
 	@Autowired
