c5132751adcb4351421dc93df540a09ba2ae4557
Xavier Schildwachter <x.schildwachter@sagebase.org>
2021-06-07 14:46:29 -0700

    Fix per review

--- lib/lib-audit/src/main/java/org/sagebionetworks/audit/kinesis/ObjectRecordLoggerImpl.java
+++ lib/lib-audit/src/main/java/org/sagebionetworks/audit/kinesis/ObjectRecordLoggerImpl.java
@@ -10,8 +10,12 @@ import java.util.List;
 @Component
 public class ObjectRecordLoggerImpl implements ObjectRecordLogger {
 
+	private AwsKinesisFirehoseLogger awsKinesisFirehoseLogger;
+
 	@Autowired
-	AwsKinesisFirehoseLogger awsKinesisFirehoseLogger;
+	public ObjectRecordLoggerImpl(AwsKinesisFirehoseLogger awsKinesisFirehoseLogger) {
+		this.awsKinesisFirehoseLogger = awsKinesisFirehoseLogger;
+	}
 
 	@Override
 	public void saveBatch(String kinesisDataStreamSuffix, List<? extends AwsKinesisLogRecord> batch) {
